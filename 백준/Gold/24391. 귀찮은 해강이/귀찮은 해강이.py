6ecf3ecdf11f90370e8f8a6c95209488367bda9a
문정환 <70272622+bazzi2548@users.noreply.github.com>
2024-09-28 18:46:43 +0900

    [Gold V] Title: 귀찮은 해강이, Time: 244 ms, Memory: 45892 KB -BaekjoonHub

--- 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.py
+++ 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.py
@@ -14,28 +14,21 @@ def union(a, b):
     b = find(b)
 
     if a > b:
-        parent[a] = parent[b]
+        parent[a] = b
     else:
-        parent[b] = parent[a]
+        parent[b] = a
 
-linked = []
 for _ in range(M):
     a, b = map(int, input().split())
-    linked.append((a, b))
     if find(a) != find(b):
         union(a, b)
-
-for a, b in linked:
-    if find(a) != find(b):
-        union(a, b)
-
 
 _list = list(map(int, input().split()))
-now = parent[_list[0]]
+now = parent[find(_list[0])]
 answer = 0
 for i in range(1, N):
-    if now != parent[_list[i]]:
+    if now != parent[find(_list[i])]:
         answer += 1
-        now = parent[_list[i]]
+        now = parent[find(_list[i])]
 
 print(answer)
