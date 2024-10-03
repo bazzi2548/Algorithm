66c10a1886a97c2b7c31432018f088b8168a216c
문정환 <70272622+bazzi2548@users.noreply.github.com>
2024-10-03 18:25:21 +0900

    [Gold IV] Title: 오큰수, Time: 1052 ms, Memory: 150872 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -8,24 +8,15 @@ stack = []
 answer = deque()
 
 for i in range(n-1, -1, -1):
-    if not stack:
-        stack.append(_list[i])
-        answer.appendleft(-1)
-        continue
-
-    if stack[-1] > _list[i]:
-        answer.appendleft(stack[-1])
-        stack.append(_list[i])
-    else:
-        while True:
-            if not stack:
-                answer.appendleft(-1)
-                stack.append(_list[i])
-                break
-            num = stack.pop()
-            if num > _list[i]:
-                answer.appendleft(num)
-                stack.append(num)
-                stack.append(_list[i])
-                break
+    while True:
+        if not stack:
+            answer.appendleft(-1)
+            stack.append(_list[i])
+            break
+        num = stack.pop()
+        if num > _list[i]:
+            answer.appendleft(num)
+            stack.append(num)
+            stack.append(_list[i])
+            break
 print(*answer)
